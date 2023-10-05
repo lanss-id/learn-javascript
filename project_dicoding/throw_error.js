f5918ed54fe0c2b012e6faee8ab6255eda246d4f
maulanakayyis354 <maulanakayyis354@gmail.com>
2023-10-05 13:56:41 +0700

    fix(bug): testing handling catch error

--- project_dicoding/throw_error.js
+++ project_dicoding/throw_error.js
@@ -1,15 +1,15 @@
-const json = '{"name": "alan", "age": 19}'
+const json = '{ "age": 19}'
 
 try{
     const user = JSON.parse(json)
+
+    if(!user.name){
+        throw new SyntaxError("'name', is required!")
+    }
 
     console.log(user.name)
     console.log(user.age)
 
 } catch (error) {
-
-    console.log(error.name)
-    console.log(error.message)
-    console.log(error.stack)
-    
+    console.log(`JSON Error: ${error.message}`)
 }
