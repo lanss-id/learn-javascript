74611476385a72193e1d64084c0747130324c467
maulanakayyis354 <maulanakayyis354@gmail.com>
2023-10-05 13:30:05 +0700

    refactor(add): add try & catch error handling

--- project_dicoding/index.js
+++ project_dicoding/index.js
@@ -10,12 +10,20 @@
 
 import {coffeStock, isCoffeMacineReady} from "./state.js";
 
-const displayStock = stock => {
-    for(const type in stock){
-        console.log(type)
+try { // coba untuk menjalankan code
+    const displayStock = stock => {
+        for(const type in stock){
+            console.log(type)
+        }
     }
+    
+    displayStock(coffeStocks)
+    
+    console.log(2+2)
+} catch (error) { // mengangkap apabila terdapat eror dalam code try
+    console.log(error.name)
+    console.log(error.message)
+    console.log(error.stack)
+} finally { // akan menjalankan code tak peduli ada eror atau tidak
+    console.log(coffeStock)
 }
-
-displayStock(coffeStock)
-
-console.log(2+2)
