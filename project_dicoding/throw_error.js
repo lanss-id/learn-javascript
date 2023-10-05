68d42f66cfa0ecdf4630fdb061924388a7d87131
maulanakayyis354 <maulanakayyis354@gmail.com>
2023-10-05 14:23:35 +0700

    fix(bug): fix catch eror handling with if statement

--- project_dicoding/throw_error.js
+++ project_dicoding/throw_error.js
@@ -1,15 +1,22 @@
-const json = '{ "age": 19}'
+const json = '{"name": "Maulana Kayyis", "age": 19}'
 
 try{
     const user = JSON.parse(json)
 
     if(!user.name){
-        throw new SyntaxError("'name', is required!")
+        throw new SyntaxError("'name', is required!") // trow error line ini tidak muncul karena tidak ada handler dari catch nya
     }
 
-    console.log(user.name)
-    console.log(user.age)
+    console.log(`Halo nama gue ${user.name}`)
+    console.log(`Umur gue ${user.age} tahun`)
 
+// buat if statement di dalam catch agar trow di line 7 bisa muncul
 } catch (error) {
-    console.log(`JSON Error: ${error.message}`)
+    if(error instanceof SyntaxError){
+        console.log(`JSON Error: ${error.message}`) // dengan instaceof dapat membuat percabangan bagaimana cara menangani eror
+    } else if(error instanceof ReferenceError){
+        console.log(error.message)
+    } else{
+        console.log(error.stack)
+    }
 }
